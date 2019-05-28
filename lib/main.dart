02ca0138af0b00d4660d7b2191b3a6c31bbf160f
fahmiyuseri <fahmiyuseri@ipay88.com.my>
2019-05-28 11:13:06 +0800

    Create Drawer

--- lib/main.dart
+++ lib/main.dart
@@ -40,9 +40,66 @@ class HomePageState extends State<HomePage>{
              currentAccountPicture: GestureDetector(
                child: new CircleAvatar(
                 backgroundColor: Colors.grey,
+                child: Icon(Icons.person, color:Colors.white),
                ),
                ),
-               )
+               decoration: new BoxDecoration(color: Colors.red  
+               ),
+               ),
+
+               //Body
+               InkWell(
+                 onTap: (){},
+                 child: ListTile( 
+                   title: Text("Home Page"),
+                   leading: Icon(Icons.home),
+                  )
+               ),
+                InkWell(
+                 onTap: (){},
+                 child: ListTile( 
+                   title: Text("My Account"),
+                   leading: Icon(Icons.person),
+                  )
+               ),
+                InkWell(
+                 onTap: (){},
+                 child: ListTile( 
+                   title: Text("My Orders"),
+                   leading: Icon(Icons.shopping_basket),
+                  )
+               ),
+                InkWell(
+                 onTap: (){},
+                 child: ListTile( 
+                   title: Text("Categories"),
+                   leading: Icon(Icons.dashboard),
+                  )
+               ),
+                 InkWell(
+                 onTap: (){},
+                 child: ListTile( 
+                   title: Text("Favourites"),
+                   leading: Icon(Icons.favorite),
+                  )
+               ),
+                
+               Divider(),
+                InkWell(
+                 onTap: (){},
+                 child: ListTile( 
+                   title: Text("Settings"),
+                   leading: Icon(Icons.settings),
+                  )
+               ),
+                InkWell(
+                 onTap: (){},
+                 child: ListTile( 
+                   title: Text("About"),
+                   leading: Icon(Icons.help),
+                  )
+               ),
+               
           ],
         ),
       ),
